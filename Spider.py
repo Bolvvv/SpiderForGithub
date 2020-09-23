c083b4dcc787a73bcfd0ca3425cd238b8ced94c2
Bolvvv <605725874@qq.com>
2020-09-23 21:49:23 +0800

    完成翻页爬取

--- Spider.py
+++ Spider.py
@@ -27,12 +27,8 @@ def download_readme(link):
         ff.close()
         return save_address
 
-#获取用户仓库信息
-def get_repositories_info(user_name):
-    repositories_url = "https://github.com/"+user_name+"?tab=repositories"
-    repositories_page_resp = requests.get(repositories_url)
-    repositories_page_soup = BeautifulSoup(repositories_page_resp.content, features="html.parser")
-    respositories_list = repositories_page_soup.find(id='user-repositories-list').find_all("li")
+#生成仓库信息
+def generate_repositories_info(respositories_list):
     #获取仓库信息
     respositories_json = []
     for i in respositories_list:
@@ -45,8 +41,38 @@ def get_repositories_info(user_name):
         temp_json['link'] = i.a['href']
         temp_json['readme_local_address'] = download_readme(i.a['href'])
         respositories_json.append(temp_json)
-    print(respositories_json)
+    return respositories_json
 
-get_repositories_info(source_user)
+#获取用户仓库信息
+def get_repositories_info(user_name):
+    repositories_url = "https://github.com/"+user_name+"?tab=repositories"
+    next_page_button_flag = True
+    final_json = []
+    while next_page_button_flag:
+        page_resp = requests.get(repositories_url)
+        soup = BeautifulSoup(page_resp.content, features="html.parser")
+        block = soup.find(id='user-repositories-list')
+        #判断仓库页是否有具体仓库
+        if block.find("ul") == None:
+            break
+        #获取当页数据
+        respositories_list = block.find_all("li")
+        final_json = final_json + generate_repositories_info(respositories_list)
+        #判断是否有下一页
+        jump_bar_nav = block.find('ul').next_sibling.next_sibling
+        if jump_bar_nav == None:
+            next_page_button_flag = False
+        else:
+            #判断“下一页”按钮是否有效
+            for next_button in list(jump_bar_nav.children)[1]:
+                continue
+            if(next_button.name == "button"):
+                next_page_button_flag = False
+            else:
+                repositories_url = next_button["href"]
 
-# following_url = "https://github.com/"+source_user+"?tab=following"
+get_repositories_info("chanchann")
+# #获取用户关注列表
+# def get_user_following(user_name):
+#     following_url = "https://github.com/"+user_name+"?tab=following"
+
