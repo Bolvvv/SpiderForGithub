957443773dc6b524e8919998da6267e40d6e1c5f
Bolvvv <605725874@qq.com>
2020-09-24 15:30:21 +0800

    完成获取用户关注列表功能

--- Spider.py
+++ Spider.py
@@ -50,7 +50,8 @@ def get_repositories_info(user_name):
     final_json = []
     while next_page_button_flag:
         page_resp = requests.get(repositories_url)
-        soup = BeautifulSoup(page_resp.content, features="html.parser")
+        #在这一步需要注意设置bs4使用的默认解析器，有html.parser,lxml和html5lib。使用html.parser容易造成解析不完全的问题，故更换为lxml
+        soup = BeautifulSoup(page_resp.content, 'lxml')
         block = soup.find(id='user-repositories-list')
         #判断仓库页是否有具体仓库
         if block.find("ul") == None:
@@ -71,8 +72,37 @@ def get_repositories_info(user_name):
             else:
                 repositories_url = next_button["href"]
 
-get_repositories_info("chanchann")
-# #获取用户关注列表
-# def get_user_following(user_name):
-#     following_url = "https://github.com/"+user_name+"?tab=following"
+# 获取用户关注列表与被关注列表
+def get_user_follow(user_name):
+    following_url = "https://github.com/"+user_name+"?tab=following"
+    next_page_button_flag = True
+    final_json = []
+    while next_page_button_flag:
+        page_resp = requests.get(following_url)
+        soup = BeautifulSoup(page_resp.content, features="lxml")
+        #获取对应关注列表块
+        follow_list = list(list(list(list(list(soup.main)[3])[1])[3])[3])[3]
+        list_a = follow_list.find_all('a', class_='d-inline-block no-underline mb-1')
+        #判断是否有关注好友
+        if list_a == []:
+            break
+        #获取当页关注列表
+        for i in list_a:
+            final_json.append(i['href'])
+        #判断是否有下一页按钮
+        button_nav = follow_list.find('div', class_ = "pagination")
+        if button_nav == None:
+            next_page_button_flag = False
+        else:
+            #判断下一页按钮是否有效
+            next_button = list(button_nav)[1]
+            if next_button.name != 'a':
+                next_page_button_flag = False
+            else:
+                following_url = next_button['href']
+    return final_json
+
+
+print(len(get_user_follow("Bolvvv")))
+
 
